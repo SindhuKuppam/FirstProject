ead673fc92ed7094d940e53425ea743a4590f9ca
SindhuKuppam <sindhu.dakaraju@gmail.com>
2021-12-26 16:48:02 -0500

    first

---
+++ src/Ex.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+public class Ex {
+    public static void main(String[] args) {
+        //ArrayList is a default class present in java
+        //dynamic array
+        ArrayList ar=new ArrayList();//raw data type
+        ar.add(10);
+        ar.add("A");
+        ar.add(30);
+        System.out.println(ar);
+        System.out.println(ar.size());
+        System.out.println(ar.get(1));
+        ArrayList<Integer> a1= new ArrayList<>(Arrays.asList(1,2,3,4));
+
+
+
+    }
+}
+
+
